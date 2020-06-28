92e5be24e1bda862cee1cd7e07936f58f9775cb3
Paul Lorett Amazona <whatevergeek@gmail.com>
2020-06-28 18:11:33 +0800

    added answers to e-ownership-references

--- exercise/e-ownership-references/src/main.rs
+++ exercise/e-ownership-references/src/main.rs
@@ -11,37 +11,61 @@ fn main() {
         .unwrap_or_else(|| {
             println!("Please supply an argument to this program.");
             std::process::exit(-1);
-        }).to_owned();
+        })
+        .to_owned();
 
     // 1. Write a function `inspect` that takes a reference to a String, returns nothing, but
     // prints whether the contents of the String is plural or singular. Then uncomment and run this
     // code with `cargo run apple` and `cargo run apples'.  Hint: use `.ends_with("s")` on the
     // String reference
     //
-    //inspect(&arg);
+    inspect(&arg);
 
     // 2. Write a function `change` that takes a *mutable* reference to a String and adds an "s" to
     // the String if it doesn't already end with "s". Then uncomment and run the code below with
     // `cargo run apple`.  Hint: use `.push_str("s")` on the mutable String reference to add an "s".
     //
-    //change(&mut arg);
-    //println!("I have many {}", arg);
+    change(&mut arg);
+    println!("I have many {}", arg);
 
     // 3. Write a function `eat` that accepts ownership of (consumes) a String and returns a bool
     // indicating whether or not the String both starts with a "b" AND contains an "a".
     // Hint 1: use `.starts_with("b")` and `.contains("a")`
     // Hint 2: `&&` is the boolean "AND" operator
     //
-    //if eat(arg) {
-    //    println!("Might be bananas");
-    //} else {
-    //    println!("Not bananas");
-    //}
+    if eat(arg) {
+       println!("Might be bananas");
+    } else {
+       println!("Not bananas");
+    }
 
     // Try running this program with "boat", "banana", and "grapes" as the arguments :-)
 
     // Challenge: Write a function "add" that takes *references* to two integer arguments,
     // dereferences them and adds them together, and returns the result.
     //
-    // println!("1 + 2 = {}, even via references", add(&1, &2));
+    println!("1 + 2 = {}, even via references", add(&1, &2));
+}
+
+fn inspect(arg: &String) {
+    print!("{}: ", arg);
+    if arg.ends_with("s") {
+        println!("plural");
+    } else {
+        println!("singular");
+    }
+}
+
+fn change(arg: &mut String) {
+    if !arg.ends_with("s"){
+        arg.push_str("s")
+    }
+}
+
+fn eat(arg: String) -> bool {
+    arg.starts_with("b") && arg.contains("a")
+}
+
+fn add(x: &i32, y: &i32) -> i32{
+    *x + *y
 }
